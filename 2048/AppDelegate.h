1ca501dbd883fe7f45227ea8d41774e44539acf0
zhemin <forevermzm00@gmail.com>
2015-05-24 15:48:41 -0700

    Initial Commit

---
+++ 2048/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2048
+//
+//  Created by MiZhemin on 5/24/15.
+//  Copyright (c) 2015 MiZhemin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2048/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2048
+//
+//  Created by MiZhemin on 5/24/15.
+//  Copyright (c) 2015 MiZhemin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
